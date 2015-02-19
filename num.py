ed648678957550cb52edb2175013aa73b09f26fc
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-02-19 18:33:33 +0100

    20150219.1833

--- num.py
+++ num.py
@@ -2,11 +2,12 @@
 import numpy
 
 C12=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(1,2))
+C13=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(1,3))
 C23=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(2,3))
 C56=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(5,6))
+
 X = C12[:,1]
 Y = C12[:,0]
-
 print "TMP36 vs. DS18B20"
 (a,b)= numpy.polyfit(X,Y,1)
 print a, b
@@ -20,6 +21,18 @@ X = C23[:,1]
 Y = C23[:,0]
 print "---"
 print "DS18B20 vs. DHT22"
+(a,b)= numpy.polyfit(X,Y,1)
+print a, b
+
+print numpy.corrcoef(X,Y)[0,1]
+
+print X[-1], a*X[-1]+b, Y[-1]
+
+
+X = C13[:,1]
+Y = C13[:,0]
+print "---"
+print "TMP36 vs. DHT22"
 (a,b)= numpy.polyfit(X,Y,1)
 print a, b
 
