d932755a52820cbfcf80a9a02e02697d8619c339
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-02-19 09:58:38 +0100

    hack to get some correlation coeffs for various temperatures

--- num.py
+++ num.py
@@ -1,11 +1,33 @@
 #! /usr/bin/python
 import numpy
 
-C=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(1,2))
-X = C[:,1]
-Y = C[:,0]
-#Y1 = numpy.vstack([Y,numpy.ones(len(Y))]).T
-#print numpy.linalg.lstsq(Y1,X)
+C12=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(1,2))
+C23=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(2,3))
+C56=numpy.loadtxt('/tmp/testser.txt',delimiter=',',usecols=(5,6))
+X = C12[:,1]
+Y = C12[:,0]
+
+(a,b)= numpy.polyfit(X,Y,1)
+print a, b
+
+print numpy.corrcoef(X,Y)[0,1]
+
+print a*X[0]+b, Y[0]
+
+
+X = C23[:,1]
+Y = C23[:,0]
+
+(a,b)= numpy.polyfit(X,Y,1)
+print a, b
+
+print numpy.corrcoef(X,Y)[0,1]
+
+print a*X[0]+b, Y[0]
+
+
+X = C56[:,1]
+Y = C56[:,0]
 
 (a,b)= numpy.polyfit(X,Y,1)
 print a, b
