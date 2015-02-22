423a8ffc545b88951d51306714643d5690842188
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-02-22 21:46:25 +0100

    20150222.2146

--- cmdMULTIsens/lib/ChipTemp/ChipTemp.h
+++ cmdMULTIsens/lib/ChipTemp/ChipTemp.h
@@ -7,7 +7,19 @@
 // Resolution 0.1 degree
 
 // Calibration values, set in decimals
-static const float offset = 319.561; // 335.2; // change this by calibration result
+
+/*
+  Calibration date 20150222:
+  offset = 335.2
+  gain = 1.06154
+  offset’ = Celsius * gain / 10 + offset – gain * T
+
+  RT=16.5
+  Celsius = 20.54
+
+  offset' = 20.54 * 1.06154 /10 + 335.2 -1.06154*16.5  =  339.4886216
+
+static const float offset = 339.489; // 335.2; // change this by calibration result
 static const float gain = 1.06154;
 
 static const int samples = 1000; // must be >= 1000, else the gain setting has no effect
