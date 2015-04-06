900bd120634a45b08479a9679bf4558869d67fb1
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-04-06 10:05:43 +0200

    20150406.1005

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-if [ "$1" = "master" ]; then
-  echo "Updating to master"
-  git fetch origin && git reset --hard origin/master && git clean -f -d
-fi
-if [ "$1" = "dev" ]; then
-  echo "Updating to dev"
-  git fetch origin && git reset --hard origin/dev && git clean -f -d
-fi
+#if [ "$1" = "master" ]; then
+#  echo "Updating to master"
+#  git fetch origin && git reset --hard origin/master && git clean -f -d
+#fi
+#if [ "$1" = "dev" ]; then
+#  echo "Updating to dev"
+#  git fetch origin && git reset --hard origin/dev && git clean -f -d
+#fi
+
+echo "Updating to"$1
+git fetch origin && git reset --hard origin/$1 && git clean -f -d
 
 # Set permissions
 sudo chmod -R 744 *
