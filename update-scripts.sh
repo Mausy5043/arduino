a7b6dbef37cd6f7b0da6f588a96b020cd5921fac
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2015-05-08 16:36:23 +0200

    better banch switching

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 echo "Updating to "$1
-git fetch origin && git reset --hard origin/$1 && git clean -f -d
+
+git fetch origin
+git checkout $1 && git reset --hard origin/$1 && git clean -f -d
 
 # Set permissions
 sudo chmod -R 744 *
