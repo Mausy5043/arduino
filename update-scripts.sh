d5ba134471f7d75f3db8800054a91955e9c33408
Mausy5043 <Mausy5043@users.noreply.github.com>
2015-08-18 10:25:43 +0200

    update script

--- update-scripts.sh
+++ update-scripts.sh
@@ -3,7 +3,13 @@
 echo "Updating to "$1
 
 git fetch origin
-git checkout $1 && git reset --hard origin/$1 && git clean -f -d
 
+ git pull
+ git fetch origin
+ git checkout $1
+ git reset --hard origin/$1 && \
+ git clean -f -d
+
+#python -m compileall .
 # Set permissions
 sudo chmod -R 744 *
