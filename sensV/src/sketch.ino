f01917665863cf53aacf6cdb0893eff1f33c9100
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-01-03 13:26:33 +0100

    add drawing

--- sensV/src/sketch.ino
+++ sensV/src/sketch.ino
@@ -6,6 +6,38 @@
   *******
   Do not connect more than 5 volts directly to an Arduino pin!!
   *******
+                                      +-----+
+         +----[PWR]-------------------| USB |--+
+         |                            +-----+  |
+         |         GND/RST2  [ ][ ]            |
+         |       MOSI2/SCK2  [ ][ ]  A5/SCL[ ] |   C5
+         |          5V/MISO2 [ ][ ]  A4/SDA[ ] |   C4
+         |                             AREF[ ] |
+         |                              GND[ ] |
+         | [ ]N/C                    SCK/13[ ] |   B5
+         | [ ]v.ref                 MISO/12[ ] |   .
+         | [ ]RST                   MOSI/11[ ]~|   .
+         | [ ]3V3    +---+               10[ ]~|   .
+         | [ ]5v     | A |                9[ ]~|   .
+         | [ ]GND   -| R |-               8[ ] |   B0
+         | [ ]GND   -| D |-                    |
+         | [ ]Vin   -| U |-               7[ ] |   D7
+         |          -| I |-               6[ ]~|   .
+         | [B]A0    -| N |-               5[ ]~|   .
+         | [ ]A1    -| O |-               4[ ] |   .
+         | [ ]A2     +---+           INT1/3[A]~|   .
+         | [ ]A3                     INT0/2[ ] |   .
+         | [ ]A4/SDA  RST SCK MISO     TX>1[ ] |   .
+         | [ ]A5/SCL  [ ] [ ] [ ]      RX<0[ ] |   D0
+         |            [ ] [ ] [ ]              |
+         |  UNO_R3    GND MOSI 5V  ____________/
+          \_______________________/
+
+      http://busyducks.com/ascii-art-arduinos
+
+      A = LED
+      B = Volt measurement
+
 */
 
 // +V from battery is connected to analog pin 0
